403466942872edb816c53414bb2ab9d0df372d23
Aditya Kanu <81138360+adityakanu@users.noreply.github.com>
2023-07-09 20:26:54 +0530

    removed keys

--- features/Auth/firebasekeys.js
+++
@@ -1,7 +0,0 @@
-export const API_KEY = "AIzaSyABGF1v0w43s84ptrzQJijen35DlpaRL5E";
-export const AUTH_DOMAIN = "finance-tracker-login.firebaseapp.com";
-export const PROJECT_ID = "finance-tracker-login";
-export const STORAGE_BUCKET = "finance-tracker-login.appspot.com";
-export const MESSAGING_SENDER_ID = "568099662115";
-export const APP_ID = "1:568099662115:web:f78ed70dd961eaf985c496";
-export const MEASUREMENT_ID = "G-F9PBE5F6MK";
